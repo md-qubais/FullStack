ee520d57d24e084e801ecdd707b61405efb25c57
Abdul Hameed <abdulhameed29r@gmail.com>
2021-08-30 14:21:57 -0700

    javascript revision

---
+++ FrontEnd/FunctionalProgramming/intro.js
@@ -0,0 +1,20 @@
+//Functional programming is a style of programming where solutions are simple,
+// isolated functions, without any side effects outside of the function scope
+
+// Functional programming is about:
+
+// Isolated functions - there is no dependence on the state of the program,
+// which includes global variables that are subject to change
+
+// Pure functions - the same input always gives the same output
+
+// Functions with limited side effects - any changes, or mutations, to the state of the program outside the function are carefully controlled
+
+
+
+
+
+
+
+
+
